63f4e394344f48ca9e92f5c99ad4cdf50a088126
Aulia Firda Syafira <auliafsyafira@gmail.com>
2024-05-27 20:06:11 +0700

    fitur keuangan

---
+++ app/Filament/Resources/KeuanganResource/Pages/EditKeuangan.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\KeuanganResource\Pages;
+
+use App\Filament\Resources\KeuanganResource;
+use Filament\Pages\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditKeuangan extends EditRecord
+{
+    protected static string $resource = KeuanganResource::class;
+
+    protected function getActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+        ];
+    }
+}
---
+++ app/Filament/Resources/KeuanganResource/Widgets/KeuanganStatsWidget.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Filament\Resources\KeuanganResource\Widgets;
+
+use Carbon\Carbon;
+use App\Models\Keuangan;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Card;
+use Illuminate\Support\Facades\Money;
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Form;
+use Filament\Forms\Components\Section;
+use BaseDashboard\Concerns\HasFiltersForm;
+use Filament\Forms\Get;
+
+class KeuanganStatsWidget extends BaseWidget
+{
+    protected function getCards(): array
+    {
+        // filter tangggal
+        $startDate = ! is_null($this->filters['startDate'] ?? null) ?
+        Carbon::parse($this->filters['startDate']) :
+        null;
+
+        $endDate = ! is_null($this->filters['endDate'] ?? null) ?
+        Carbon::parse($this->filters['endDate']) :
+        now();
+
+        // widget total
+        $pemasukan = Keuangan::where('jenis', 'Masuk')->sum('nominal');
+        $pengeluaran = Keuangan::where('jenis', 'Keluar')->sum('nominal');
+
+        return [
+            Card::make('Total Anggaran', $this->formatRupiah ($pemasukan - $pengeluaran)),
+            Card::make('Total Pemasukan', $this->formatRupiah ($pemasukan)),
+            Card::make('Total Pengeluaran', $this->formatRupiah ($pengeluaran)),
+        ];
+    }
+
+    public function formatRupiah($amount)
+    {
+        return 'Rp' . number_format($amount, 2, ',', '.');
+    }
+
+}
+
