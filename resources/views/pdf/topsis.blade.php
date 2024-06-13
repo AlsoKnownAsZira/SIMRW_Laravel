d3f57d72aad9a4c3520456231590d493a1f4c593
Riska Kurnia Triwulandari <riskakurniatriwulandari@gmail.com>
2024-06-14 01:01:45 +0700

    View Surat Bansos

--- resources/views/pdf/topsis.blade.php
+++ resources/views/pdf/topsis.blade.php
@@ -12,6 +12,7 @@ $imageBase64 = 'data:image/jpg;base64,' . $imageData;
 <!DOCTYPE html>
 <html>
 <head>
+    <title>Surat Pernyataan</title>
     <style>
         body {
             font-family: 'Times New Roman', Times, serif;
@@ -22,7 +23,6 @@ $imageBase64 = 'data:image/jpg;base64,' . $imageData;
             display: flex;
             align-items: center;
             margin-bottom: 20px;
-            /* text-align: center; */
         }
 
         .header img.logo {
@@ -58,6 +58,12 @@ $imageBase64 = 'data:image/jpg;base64,' . $imageData;
             border-collapse: collapse;
         }
 
+        th, td {
+            border: 1px solid black;
+            padding: 8px;
+            text-align: left;
+        }
+
         .table-no-border, .table-no-border th, .table-no-border td {
             border: none;
         }
@@ -70,13 +76,15 @@ $imageBase64 = 'data:image/jpg;base64,' . $imageData;
 
 </head>
 <body>
-<div class="container">
-    <div class="header">
-        <img class="logo" src="{{ $imageBase64 }}" alt="Logo" />
-        <div class="header-text">
-            <h4>PEMERINTAH DESA JATIMULYO</h4>
-            <h4>KECAMATAN LOWOKWARU KOTA MALANG</h4>
-            <h3><b>RUKUN WARGA 05</b></h3>
+<div class="header">
+    <div class="kopsurat">
+        <div class="header">
+            <img class="logo" src="{{ $imageBase64 }}" alt="Logo" />
+            <div class="header-text">
+                <h4>PEMERINTAH DESA JATIMULYO</h4>
+                <h4>KECAMATAN LOWOKWARU KOTA MALANG</h4>
+                <h3><b>RUKUN WARGA 05</b></h3>
+            </div>
         </div>
     </div>
     <hr>
@@ -84,75 +92,85 @@ $imageBase64 = 'data:image/jpg;base64,' . $imageData;
         <h3><u>Surat Pernyataan</u></h3>
         <h3><u>Bantuan Sosial</u></h3>
     </div>
-    <div class="content">
-        <p>Perihal  : Penerima Bantuan Sosial</p>
-        <p>Lampiran : 1</p>
+</div>
+<div class="content">
+    <p>Perihal  : Penerima Bantuan Sosial</p>
+    <p>Lampiran : 1</p>
 
-        <p>Kepada Yth,</p>
-        <p>Bapak/Ibu Warga RW 05</p>
-        <p>Di Tempat</p>
-        <p>Dengan Hormat,</p>
-        <p>
-            Dengan ini kami selaku pengurus Bantuan Sosial Warga RW 05 memberitahukan bahwa yang berhak menerima
-            Bantuan Sosial adalah sebagai berikut:
-        </p>
-        <h2>Nama: {{ $nama }}</h2>
+    <Bapak>Kepada Yth, Bapak/Ibu Warga RW 05</p>
+    <p>Di Tempat</p>
+    <p>Dengan Hormat,</p>
+    <p>
+        Dengan ini kami selaku pengurus Bantuan Sosial RW 05 menyatakan bahwa yang berhak menerima
+        Bantuan Sosial:
+    </p>
+    <ul>
+        <li>Tidak memiliki penghasilan tetap atau memiliki penghasilan yang tidak mencukupi untuk memenuhi kebutuhan dasar sehari-hari.</li>
+        <li>Bertanggung jawab atas tanggungan keluarga.</li>
+        <li>Tinggal di rumah dengan kondisi fisik yang tidak layak huni.</li>
+        <li>Membayar biaya listrik per bulan yang cukup membebani keuangan keluarga.</li>
+        <li>Bekerja dengan penghasilan yang tidak mencukupi untuk kehidupan sehari-hari.</li>
+        <li>Berusia yang tergolong dalam kelompok rentan.</li>
+        <li>Memiliki maupun tidak memiliki cacat fisik.</li>
+    </ul>
 
-        <p>Demikian surat ini kami sampaikan. Atas perhatian dan kehadirannya kami ucapkan terimakasih.</p>
+    <Demikian>Berdasarkan kondisi tersebut, dengan ini menyatakan bahwa orang berikut berhak untuk menerima bantuan sosial dari RW 05
+        untuk membantu meringankan beban ekonomi keluarga.Demikian surat pernyataan ini dibuat dengan sebenar-benarnya untuk digunakan sebagaimana mestinya.</p>
 
-        <div class="signature">
-            Mengetahui,<br>
-            Ketua RW 05<br><br><br><br>
-            .................................
-        </div>
-    </div>
+    <table class="table-no-border">
+        <tr>
+            <td class="signature">
+                Mengetahui,<br>
+                Ketua RW 05<br><br><br><br>
+                .................................
+            </td>
+        </tr>
+    </table>
+</div>
 
     <div class="page-break"></div>
 
-    <div class="content">
-        <h1>Lampiran: Hasil Perhitungan TOPSIS</h1>
-        @foreach ($alternatifData as $altData)
-            <h2>Alternatif: {{ $altData['nama'] }}</h2>
+    <h1>Lampiran: Hasil Perhitungan TOPSIS</h1>
+    @foreach ($alternatifData as $data)
+        <h2>Alternatif: {{ $data['nama'] }}</h2>
 
-            <!-- Nilai Preferensi -->
-            <h3>Nilai Preferensi dan Rangking</h3>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Nilai Preferensi</th>
-                        <th>Rangking</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>{{ $altData['nilai_preferensi'] }}</td>
-                        <td>{{ $altData['rangking'] }}</td>
-                    </tr>
-                </tbody>
-            </table>
-        @endforeach
-
-        <!-- Solusi Ideal -->
-        <h2>Solusi Ideal</h2>
+        <!-- Nilai Preferensi -->
+        <h3>Nilai Preferensi dan Rangking</h3>
         <table>
             <thead>
                 <tr>
-                    <th>Kriteria</th>
-                    <th>Ideal Positif</th>
-                    <th>Ideal Negatif</th>
+                    <th>Nilai Preferensi</th>
+                    <th>Rangking</th>
                 </tr>
             </thead>
             <tbody>
-                @foreach ($kriterias as $kriteria)
-                    <tr>
-                        <td>{{ $kriteria->nama }}</td>
-                        <td>{{ $ideal_positif[$kriteria->id] }}</td>
-                        <td>{{ $ideal_negatif[$kriteria->id] }}</td>
-                    </tr>
-                @endforeach
+                <tr>
+                    <td>{{ $data['nilai_preferensi'] }}</td>
+                    <td>{{ $data['rangking'] }}</td>
+                </tr>
             </tbody>
         </table>
-    </div>
-</div>
+    @endforeach
+
+    <!-- Solusi Ideal -->
+    <h2>Solusi Ideal</h2>
+    <table>
+        <thead>
+            <tr>
+                <th>Kriteria</th>
+                <th>Ideal Positif</th>
+                <th>Ideal Negatif</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($kriterias as $kriteria)
+                <tr>
+                    <td>{{ $kriteria->nama }}</td>
+                    <td>{{ $ideal_positif[$kriteria->id] }}</td>
+                    <td>{{ $ideal_negatif[$kriteria->id] }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 </body>
 </html>
